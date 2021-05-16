1079de7315da0f90cc213d76489cf90079bd4a19
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-16 14:10:39 +0200

    add AllCoutryScores component

---
+++ src/AllCountryScores.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Scores } from './Scores';
+
+function AllCountryScores() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllCountryScores
+
+
