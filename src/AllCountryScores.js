4725dc29a2155c372ad2c56059cca9678c88df47
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-18 10:14:38 +0200

    add function for counrty and player info

--- src/AllCountryScores.js
+++ src/AllCountryScores.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import { Scores } from './Scores';
+import { Data } from "./Data";
 
 function AllCountryScores() {
   return (
-    <div>
-      
+    <div className="page">
+      <div className="heading">
+        <h1>High Scores per Country</h1>
+      </div>
+      {Data.map((country) => {
+        return (
+          <section className="info">            
+              <Country CountryName={country.name} style={{ backgroundColor: "red" }} />
+              <PlayerNames Names={country.scores} />
+          </section>
+        )
+      })}
     </div>
   )
 }
 
+function Country({ CountryName }) {
+  return (
+    <div className="country">
+      <h3>HIGH SCORES: {CountryName}</h3>
+    </div>
+  )
+}
+
+function PlayerNames({ Names }) {
+  return(
+  Names.map(el => {
+    return (
+      <div className="players">
+        <p>{el.n} {el.s}</p>
+      </div>
+    )
+  })
+  )
+}
+
 export default AllCountryScores
-
-
