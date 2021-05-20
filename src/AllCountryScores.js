ddd8bd5462cceec620cfa601238fda8e90c17306
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-20 21:22:51 +0200

    change styling

--- src/AllCountryScores.js
+++ src/AllCountryScores.js
@@ -5,14 +5,14 @@ import PlayerNames from "./PlayerNames";
 
 function AllCountryScores() {
   return (
-    <div style={{ border: "solid .5rem blue" }}>
-      <div style={{ border: "solid .5rem red" }} className="heading">
+    <div style={{ border: "solid .3rem blue" }}>
+      <div style={{ border: "solid .3rem red" }} className="heading">
         <h1>High Scores per Country</h1>
       </div>
       {Data.map((country, index) => {
         return (
-          <section key={index} style={{ border: "solid .25rem green" }}className="info">
-              <Country CountryName={country.name} style={{ backgroundColor: "red" }} />
+          <section key={index} style={{ border: ".1rem", padding: "1rem", border: "solid .2rem green" }}className="info">
+              <Country CountryName={country.name} style={{ padding: "1rem", backgroundColor: "red" }} />
               <PlayerNames Names={country.scores} />
           </section>
         )
