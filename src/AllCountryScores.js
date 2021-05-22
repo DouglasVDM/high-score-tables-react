3a933a010d90b3d6657d919f786353b2c86e27ab
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-22 20:10:17 +0200

    sort country names in alphabetical order

--- src/AllCountryScores.js
+++ src/AllCountryScores.js
@@ -4,14 +4,32 @@ import Country from "./Country";
 import PlayerNames from "./PlayerNames";
 
 function AllCountryScores() {
+
+  let compareDescendingOrder = (object1, object2) => {
+    const obj1 = object1
+    const obj2 = object2
+
+    if (obj1 < obj2) {
+      return -1
+    }
+    if (obj1 > obj2) {
+      return 1
+    }
+    return 0
+  }
+
+
   return (
     <div style={{ border: "solid .3rem blue" }}>
       <div style={{ border: "solid .3rem red" }} className="heading">
         <h1>High Scores per Country</h1>
       </div>
-      {Data.map((country, index) => {
+      {Data
+        .sort((name1, name2) => compareDescendingOrder(name1.name, name2.name)    
+        )
+        .map((country, index) => {
         return (
-          <section key={index} style={{ border: ".1rem", padding: "1rem", border: "solid .2rem green" }}className="info">
+          <section key={index} style={{ padding: "1rem", border: "solid .2rem green" }}className="info">
               <Country CountryName={country.name} style={{ padding: "1rem", backgroundColor: "red" }} />
               <PlayerNames Names={country.scores} />
           </section>
