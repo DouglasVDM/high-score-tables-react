c423fd485b4fca58cbdcf24c83ae72b8eaf42771
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-05-23 21:45:12 +0200

    level 4 useState button event to switch ascending/descending scores

--- src/AllCountryScores.js
+++ src/AllCountryScores.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Data } from "./Data";
 import Country from "./Country";
 import PlayerNames from "./PlayerNames";
 
 function AllCountryScores() {
+
+  const[sortOrder, setSortOrder] = useState(false)
 
   let compareDescendingOrder = (object1, object2) => {
     const obj1 = object1
@@ -19,11 +21,21 @@ function AllCountryScores() {
   }
 
 
+  const handleToggle = () => {
+    setSortOrder(!sortOrder)
+  }
+
+
   return (
     <div style={{ border: "solid .3rem blue" }}>
+      
       <div style={{ border: "solid .3rem red" }} className="heading">
+        
+        <button onClick={handleToggle}>Toggle Sort Scores</button>
+
         <h1>High Scores per Country</h1>
       </div>
+
       {Data
         .sort((name1, name2) => compareDescendingOrder(name1.name, name2.name)    
         )
@@ -31,7 +43,7 @@ function AllCountryScores() {
         return (
           <section key={index} style={{ padding: "1rem", border: "solid .2rem green" }}className="info">
               <Country CountryName={country.name} style={{ padding: "1rem", backgroundColor: "red" }} />
-              <PlayerNames Names={country.scores} />
+              <PlayerNames sortOrder={sortOrder} Names={country.scores} />
           </section>
         )
       })}
--- src/PlayerNames.js
+++ src/PlayerNames.js
@@ -1,40 +1,37 @@
 import React from 'react'
 
+function PlayerNames({ Names, sortOrder }) {
 
-
-
-function PlayerNames({ Names }) {
-
+  console.log(sortOrder);
 
   const capitaliseFirstLetter = (string) => (
     string.length ? string.charAt(0).toUpperCase() + string.slice(1) : string
   )
 
 
-  const compareDescendingOrder = (object1, object2, key) => {
+  const sortItems = (object1, object2, key, ascending = true) => {
     const obj1 = Number(object1[key])
     const obj2 = Number(object2[key])
 
     if (obj1 < obj2) {
-      return 1
+      return ascending ? -1 : 1
     }
     if (obj1 > obj2) {
-      return -1
+      return ascending ? 1 : -1
     }
     return 0
   }
-
+   
   return (
-    Names
-      .sort((score1, score2) => (
-        compareDescendingOrder(score1, score2, "s")
-      ))
+    Names.sort((score1, score2) => (
+      sortItems(score1, score2, "s", sortOrder)))
+        
       .map((el, index) => (
         <div
           key={index}
           style={{ border: "solid .1rem orange" }}
           className="players">
-          <p>{capitaliseFirstLetter( el.n )} {el.s}</p>
+          <p>{capitaliseFirstLetter( el.n )} {el.s}</p>          
         </div>
       )
     )
